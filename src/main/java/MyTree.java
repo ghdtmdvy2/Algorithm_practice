e0f808c455d484364be4344934c37568541d6020
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-08-24 21:59:43 +0900

    Tree seach 추가

--- src/main/java/MyTree.java
+++ src/main/java/MyTree.java
@@ -1,3 +1,4 @@
+
 public class MyTree {
     Node head = null;
     public class Node{
@@ -17,15 +18,19 @@ public class MyTree {
             while(true){
                 // 현재 노드 값보다 큰 경우 왼쪽으로 큰 경우 오른쪽으로 저장
                 if (node.value > value){
-                    node = node.left;
-                    if (node == null) {
-                        node = new Node(value);
+                    if (node.left != null){
+                        node = node.left;
+                    }
+                    else {
+                        node.left = new Node(value);
                         break;
                     }
                 } else {
-                    node = node.right;
-                    if (node == null) {
-                        node = new Node(value);
+                    if (node.right != null){
+                        node = node.right;
+                    }
+                    else {
+                        node.right = new Node(value);
                         break;
                     }
                 }
@@ -33,12 +38,31 @@ public class MyTree {
         }
         return true;
     }
-
+    public Node search(int value){
+        if (head == null){
+            return null;
+        }
+        else {
+            Node node = head;
+            while(node != null) {
+                if (node.value == value) {
+                    return node;
+                } else if (node.value > value) {
+                    node = node.left;
+                } else {
+                    node = node.right;
+                }
+            }
+            return null;
+        }
+    }
     public static void main(String[] args) {
         MyTree myTree = new MyTree();
         myTree.insertNode(2);
         myTree.insertNode(3);
         myTree.insertNode(4);
         myTree.insertNode(6);
+        Node testNode = myTree.search(3);
+        System.out.println(testNode.value);
     }
 }
