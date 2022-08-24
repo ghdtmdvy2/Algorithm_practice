b18d77770b94d1bef49fd1c270e5c794dab478f3
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-08-24 21:33:47 +0900

    Tree insertNode 추가

--- src/main/java/MyTree.java
+++ src/main/java/MyTree.java
@@ -1,3 +1,44 @@
 public class MyTree {
+    Node head = null;
+    public class Node{
+        Node left;
+        Node right;
+        int value;
+        public Node (int value){
+            this.value = value;
+        }
+    }
+    public boolean insertNode(int value){
+        if (head == null){
+            head = new Node(value);
+        }
+        else {
+            Node node = head;
+            while(true){
+                // 현재 노드 값보다 큰 경우 왼쪽으로 큰 경우 오른쪽으로 저장
+                if (node.value > value){
+                    node = node.left;
+                    if (node == null) {
+                        node = new Node(value);
+                        break;
+                    }
+                } else {
+                    node = node.right;
+                    if (node == null) {
+                        node = new Node(value);
+                        break;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 
+    public static void main(String[] args) {
+        MyTree myTree = new MyTree();
+        myTree.insertNode(2);
+        myTree.insertNode(3);
+        myTree.insertNode(4);
+        myTree.insertNode(6);
+    }
 }
