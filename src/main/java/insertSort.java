a48076649e25080b0109a4c0b01d96e3392429f2
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-08-31 20:56:10 +0900

    선택정렬 구현

---
+++ src/main/java/insertSort.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class insertSort {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++){
+            arrayList.add((int)(Math.random() * 100));
+        }
+
+        for (int i = 0; i < arrayList.size(); i++){
+            for (int j = i + 1; j < arrayList.size(); j++){
+                if (arrayList.get(i) < arrayList.get(j)){
+                    Collections.swap(arrayList,i,j);
+                }
+            }
+        }
+        for (int i = 0; i < 100; i++){
+            System.out.println(arrayList.get(i));
+        }
+    }
+}
