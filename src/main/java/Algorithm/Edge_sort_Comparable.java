4a2adcf2cd65805c5e5a0b448ca09ba179d8cc65
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-09-22 15:04:50 +0900

    클래스 정렬 기준을 정하기 위한 Comparator 구현

---
+++ src/main/java/Algorithm/Edge_sort_Comparable.java
@@ -0,0 +1,33 @@
+package Algorithm;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Edge_sort_Comparable {
+    Integer distance;
+    String vertex;
+
+    Edge_sort_Comparable(Integer distance, String vertex){
+        this.distance = distance;
+        this.vertex = vertex;
+    }
+    public static void main(String[] args) {
+        Edge_sort_Comparable edge1 = new Edge_sort_Comparable(13,"A");
+        Edge_sort_Comparable edge2 = new Edge_sort_Comparable(10,"A");
+        Edge_sort_Comparable edge3 = new Edge_sort_Comparable(12,"A");
+        Edge_sort_Comparable[] Edge = new Edge_sort_Comparable[]{edge1,edge2,edge3};
+        // Edge에 대한 정렬을 시도하자 distance와 vertex 중 어떤 것이 정렬 기준인지 몰라 Exception 에러가 뜸.
+        // 그래서 정렬 기준을 정하고자 두가지 방법이 있음 Comparator, Comparable
+        // 1. Comparable 클래스 안에 interface 를 정의하여 오버라이드 하는 경우.
+        // 2. Comparator Arrays.sort 메서드에서 두 번째 파라미터로 객체 생성하여 오버라이드 하는 경우.
+        Arrays.sort(Edge, new Comparator<Edge_sort_Comparable>() {
+            @Override
+            public int compare(Edge_sort_Comparable o1, Edge_sort_Comparable o2) {
+                return o1.distance - o2.distance;
+            }
+        });
+        for (int i = 0; i<Edge.length; i++){
+            System.out.println(Edge[i].distance);
+        }
+    }
+}
