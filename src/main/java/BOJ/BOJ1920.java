3a1a3bcb7e23a1aece0847cc6f085b229657cce2
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-10-26 21:52:08 +0900

    백준 1920번

---
+++ src/main/java/BOJ/BOJ1920.java
@@ -0,0 +1,41 @@
+package BOJ;
+
+//https://www.acmicpc.net/problem/1920
+import java.util.*;
+
+class BOJ1920{
+    public static int binarySearch(ArrayList<Integer> arrayList, Integer searchItem){
+        if(arrayList.size() == 1 && arrayList.get(0) == searchItem){
+            return 1;
+        }
+        if (arrayList.size() == 1 && arrayList.get(0) != searchItem){
+            return 0;
+        }
+        if (arrayList.size() == 0){
+            return 0;
+        }
+        int midSize = arrayList.size() / 2;
+        if (arrayList.get(midSize) == searchItem){
+            return 1;
+        }
+        if (arrayList.get(midSize) > searchItem){
+            return binarySearch(new ArrayList<>(arrayList.subList(0,midSize)),searchItem);
+        } else {
+            return binarySearch(new ArrayList<>(arrayList.subList(midSize,arrayList.size())),searchItem);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        for (int i = 0; i<N; i++){
+            list1.add(sc.nextInt());
+        }
+        N = sc.nextInt();
+        Collections.sort(list1);
+
+        for (int i = 0; i<N; i++){
+            System.out.println(binarySearch(list1,sc.nextInt()));
+        }
+    }
+}
