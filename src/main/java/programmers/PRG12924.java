e7e40d88ecf2930fbc5cfa96e8dbe4896d2eb279
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2023-02-21 10:20:49 +0900

    숫자의 표현

--- src/main/java/programmers/PRG12924.java
+++ src/main/java/programmers/PRG12924.java
@@ -1,4 +1,23 @@
 package programmers;
 
 public class PRG12924 {
+    public int solution(int n) {
+        int value = n;
+        int answer = 0;
+        while (value > 0) {
+            int finnValue = 0;
+            for (int i = value; i>=1; i--){
+                finnValue += i;
+                if (finnValue == n) {
+                    answer++;
+                    break;
+                }
+                else if (finnValue > n) {
+                    break;
+                }
+            }
+            value--;
+        }
+        return answer;
+    }
 }
